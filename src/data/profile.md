23b5e04fa6c9cd55712c3059c2205e5fb22bafda
James Johnson <johnsojr@gmail.com>
2016-03-12 00:08:48 -0600

    load profile from markdown

---
+++ src/data/profile.md
@@ -0,0 +1,3 @@
+## About
+
+James Johnson (M.Ed. / University of Minnesota) is currently a teacher, developer, and data-geek for the [Minneapolis College of Art and Design](http://mcad.edu). He trains faculty, administers and integrates systems, and helps build online programs including MCAD's Interactive Design and Marketing Post-Baccalaureate, where he teaches Web Development. He also does a bit of freelance stuff for fun on the side.
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,17 @@ import ProjectList from '../components/ProjectList';
 import '../styles/main.scss';
 import navIcon from '../img/icon-menu.svg';
 
+// data
+import profile from 'html!markdown!../data/profile.md';
+
+function getCurrentYear() {
+  return (new Date()).getFullYear();
+}
+
+function getHTML(str) {
+  return { __html: str };
+}
+
 function HomePage() {
   return (
     <div className="container">
@@ -18,7 +29,17 @@ function HomePage() {
         </ul>
       </header>
       <ProjectList />
+      <section className="page-section section-profile"
+        dangerouslySetInnerHTML={ getHTML(profile) }
+      />
       <footer className="page-footer">
+        <p>copyright { getCurrentYear() }</p>
+        <ul className="social-media">
+          <li><i className="fa fa-github"></i></li>
+          <li><i className="fa fa-twitter"></i></li>
+          <li><i className="fa fa-linkedin"></i></li>
+          <li><i className="fa fa-envelope"></i></li>
+        </ul>
       </footer>
     </div>
   );
